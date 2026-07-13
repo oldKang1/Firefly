0e31d10f2eccb885a41192ba2931875c7daa82d4
oldKang1 <lyklyklyk3@gmail.com>
2026-07-13 11:07:15 +0800

    Update navBarConfig.ts

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -78,23 +78,12 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		// 子菜单
 		children: [
 			{
-				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
+				name: "408杂货铺",
+				url: "https://www.csgraduates.com/data_structure/",
 				external: true,
 				icon: "fa7-brands:github",
 			},
-			{
-				name: "Gitee",
-				url: "https://gitee.com/CuteLeaf/Firefly",
-				external: true,
-				icon: "fa7-brands:gitee",
-			},
-			{
-				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
-				external: true,
-				icon: "fa7-brands:qq",
-			},
+			
 			{
 				name: "Firefly文档",
 				url: "https://docs-firefly.cuteleaf.cn",
